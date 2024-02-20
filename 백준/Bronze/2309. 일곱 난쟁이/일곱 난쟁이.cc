2cdd28c520df761a3e1f71298cbbd48fa1fe5ac4
SeungHyun Hong <126440235+strurao@users.noreply.github.com>
2024-02-20 22:06:32 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+
+int a[9];
+int main() {
+	for (int i = 0; i < 9; i++) {
+		cin >> a[i];
+	}
+	sort(a, a + 9); //난장이들의 키를 오름차순으로 정렬한다. 
+	do {
+		int sum = 0;
+		for (int i = 0; i < 7; i++) sum += a[i];
+		if (sum == 100) break;
+	} while (next_permutation(a, a + 9));
+	for (int i = 0; i < 7; i++) cout << a[i] << "\n";
+	return 0;
+}
