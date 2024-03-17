98612e06e3ea49e8e4a41fa403c794f774520364
SeungHyun <126440235+strurao@users.noreply.github.com>
2024-03-17 09:55:29 +0900

    [level 2] Title: 더 맵게, Time: 225.37 ms, Memory: 40.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+ 
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
+    while(pq.size()>1 && pq.top()<K)
+    {
+        int one=pq.top();
+        pq.pop();
+        int two=pq.top();
+        pq.pop();
+        int three=one+two*2;
+        pq.push(three);
+        answer++;
+    }
+    if(pq.top()<K) return-1;
+    else return answer;
+}
