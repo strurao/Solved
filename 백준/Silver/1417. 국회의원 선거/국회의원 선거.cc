658b6cf6f9a8e18e70f513e644ebae0bc0557852
Seunghyun Hong <126440235+strurao@users.noreply.github.com>
2024-06-17 23:48:17 +0900

    [Silver V] Title: 국회의원 선거, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.cc
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.cc
@@ -1,41 +1,24 @@
-// 다솜이보다 표수가 많은 후보들의 표를, 다솜이의 표가 많아질 때까지 매수
+#include <iostream>
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int votes[104];
-
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    int cnt =0;
-    int people;
-    int ds;
-    
-    cin >> people;
-    cin >> ds;
-    
-    for(int i=0; i<people-1; i++){
-        cin >> votes[i];
+int main()
+{
+    priority_queue<int> pq;   
+    int N, answer=0, dasom, in;
+    cin>>N;
+    cin >>dasom;
+    N--;
+    while(N--){
+        cin>>in;
+        if(in>=dasom) pq.push(in);
     }
-    
-    while(true){
-        int maxvote =0;
-        int idx = 0;
-        
-        for(int i=0; i<people-1; i++){
-            if(maxvote < votes[i]){
-                maxvote = votes[i];
-                idx=i;
-            }
-        }
-        if(ds > maxvote){
-            cout << cnt << '\n';
-            break;
-        }
-        ds++;
-        votes[idx]--;
-        cnt++;
+    while(!pq.empty() && pq.top()>=dasom ){// top이 다솜보다 크거나 같다면 
+        int t=pq.top(); pq.pop();
+        t--; answer++; dasom++;
+        pq.push(t);
     }
+    cout<<answer<<"\n";
+    return 0;
 }
